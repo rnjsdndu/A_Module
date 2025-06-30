74fc4c75b543d027ee83977b6029593a9635207f
rnjsdndu <aarnjsdn@naver.com>
2025-06-30 14:12:17 +0900

    B하나 끝

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,41 @@
-const motto = document.querySelector('.motto')
-const 
+const mottos = [...document.querySelectorAll(".motto ul li")];
+const dess = [...document.querySelectorAll(".dess div")];
+
+mottos.forEach((motto, index) => {
+
+  motto.addEventListener("mouseover", (e1) => {
+
+    dess[index].style.display = "block";
+
+    e1.currentTarget.querySelector('p').style.display = "block";
+
+    const currentBgImage = `url(./assets/images/${motto.classList[0]}.png)`;
+    mottos.forEach(item => {
+      item.style.backgroundImage = currentBgImage;
+    });
+  });
+
+  motto.addEventListener("mouseleave", (e1) => {
+
+    dess[index].style.display = "none";
+
+    e1.currentTarget.querySelector('p').style.display = "none";
+
+    mottos.forEach(item => {
+      item.style.backgroundImage = `url(./assets/images/${item.classList[0]}.png)`;
+    })
+  })
+});
+
+
+const controlor = [...document.querySelectorAll('.control li')]
+
+controlor.forEach(e=>{
+  controlor[0].addEventListener('click' , ()=>{
+    
+  })
+})
+
 
 
 
